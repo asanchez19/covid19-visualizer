f564d44614c9420c667a7429ac39c24d477b3813
Alejandro Sanchez <rasso92@gmail.com>
2020-03-29 18:02:07 -0600

    Add basic dependencies to start the project

---
+++ src/views/Index.vue
@@ -0,0 +1,19 @@
+<template>
+	<div
+		class="alert alert-success alert-dismissible fade show text-center"
+		role="alert"
+	>
+		Covid-19 Costa Rica
+	</div>
+</template>
+
+<script>
+export default {
+	/**
+	 * The component's name used for debugging.
+	 *
+	 * @type {String}
+	 */
+	name: 'Index',
+}
+</script>
