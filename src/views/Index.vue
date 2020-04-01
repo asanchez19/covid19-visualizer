aaea3454173178e9b7f51dd217a92b16836289f8
Alejandro Sanchez <rasso92@gmail.com>
2020-03-31 19:47:28 -0600

    Show the covid 19 graph

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,19 +1,196 @@
 <template>
-	<div
-		class="alert alert-success alert-dismissible fade show text-center"
-		role="alert"
-	>
-		Covid-19 Costa Rica
+	<div class="container">
+		<div style="height: 200px;" class="text-center">
+			<h1>Covid-19 Costa Rica vs {{ country }}</h1>
+			<select v-model="country" @change="onCountryChange">
+				<option v-for="element in getCountries" :key="element">
+					{{ element }}
+				</option>
+			</select>
+
+			Acción
+			<select v-model="type" @change="onTypeChange">
+				<option value="confirmed">
+					Confirmados
+				</option>
+				<option value="deaths">
+					Muertes
+				</option>
+				<option value="recovered">
+					Recuperados
+				</option>
+			</select>
+			<graph-line
+				:width="600"
+				:height="400"
+				:shape="'normal'"
+				:axis-min="0"
+				:axis-max="getTypeMax"
+				:axis-full-mode="true"
+				:labels="getDates"
+				:names="names"
+				:values="values"
+			>
+				<note :text="'Line Chart'"></note>
+				<tooltip :names="names" :position="'right'"></tooltip>
+				<legends :names="names"></legends>
+				<guideline :tooltip-y="true"></guideline>
+			</graph-line>
+		</div>
 	</div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+	/**
+	 * The component's computed properties.
+	 *
+	 * @type {Object}
+	 */
+	computed: {
+		/**
+		 * Get the list of available countries.
+		 *
+		 * @returns {Array}
+		 */
+		getCountries() {
+			if (!this.dataSet) {
+				return []
+			}
+
+			return Object.keys(this.dataSet)
+		},
+
+		/**
+		 * Get the date's array.
+		 *
+		 * @return {Array}
+		 */
+		getDates() {
+			if (!this.dataSet) {
+				return []
+			}
+
+			return this.slice(
+				this.dataSet['Costa Rica'].map((data) => data.date)
+			)
+		},
+
+		/**
+		 * Get the max number of cases in the selected type.
+		 *
+		 * @return {Number}
+		 */
+		getTypeMax() {
+			if (this.values.length === 0) {
+				return
+			}
+
+			const first = this.values[0][this.values[0].length - 1]
+			const second = this.values[1][this.values[1].length - 1]
+
+			return first > second ? first + this.limit : second + this.limit
+		},
+	},
+
+	/**
+	 * The component's local methods.
+	 *
+	 * @type {Object}
+	 */
+	methods: {
+		/**
+		 * Get the specific data.
+		 *
+		 * @return {void}
+		 */
+		getData() {
+			this.values = []
+
+			this.values.push(
+				this.slice(
+					this.dataSet['Costa Rica'].map((data) => data[this.type])
+				)
+			)
+
+			this.values.push(
+				this.slice(
+					this.dataSet[this.country].map((data) => data[this.type])
+				)
+			)
+		},
+
+		/**
+		 * Slice the array.
+		 *
+		 * @param {Array} array
+		 * @return {Array}
+		 */
+		slice(array) {
+			return array.slice(array.length - this.limit, array.length)
+		},
+
+		/**
+		 * Handle the on change event.
+		 *
+		 * @return {void}
+		 */
+		onCountryChange() {
+			this.names.pop()
+
+			this.names.push(this.country)
+
+			this.getData()
+		},
+
+		/**
+		 * Handle the on change type event.
+		 *
+		 * @return {void}
+		 */
+		onTypeChange() {
+			this.getData()
+		},
+	},
+
 	/**
 	 * The component's name used for debugging.
 	 *
 	 * @type {String}
 	 */
 	name: 'Index',
+
+	/**
+	 * The component's created lifecycle hook.
+	 *
+	 * @return {void}
+	 */
+	async created() {
+		const response = await axios.get(
+			'https://pomber.github.io/covid19/timeseries.json'
+		)
+
+		this.dataSet = response.data
+
+		this.getData()
+	},
+
+	/**
+	 * Get the component's initial state.
+	 *
+	 * @return {Object}
+	 */
+	data() {
+		return {
+			country: 'Panama',
+			dataSet: null,
+			limit: 10,
+			names: ['Costa Rica', 'Panama'],
+			type: 'confirmed',
+			values: [],
+		}
+	},
 }
 </script>
